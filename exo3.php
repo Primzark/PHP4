0a4c2cca194a1078688ac82c27dec1119908e771
Primzark <starlod7696@gmail.com>
2025-02-21 12:05:46 +0100

    done

--- exo3.php
+++ exo3.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Produits</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -14,116 +14,36 @@
         <h1 class="text-center my-5">Liste des Produits</h1>
 
         <?php
-       
-        $produits = [
-            [
-                "categorie" => "Voyage",
-                "nom" => "PRO BAG",
-                "prix" => 89.99,
-                "description" => "Sac à dos anti-vol.",
-                "image" => "./bag.jpg"
-            ],
-            [
-                "categorie" => "Jeux vidéo",
-                "nom" => "Assassin's Creed Odyssey",
-                "prix" => 59.99,
-                "description" => "Exploration ouverte dans l'ancienne Grèce.",
-                "image" => "./assasins.jpg"
-            ],
-            [
-                "categorie" => "Maison",
-                "nom" => "Oreiller",
-                "prix" => 49.99,
-                "description" => "Très confortable",
-                "image" => "./pillow.jpg"
-            ],
-            [
-                "categorie" => "Art",
-                "nom" => "Tablette Graphique",
-                "prix" => 199.99,
-                "description" => "Tablette graphique haute précision.",
-                "image" => "./tab.jpg"
-            ],
-            [
-                "categorie" => "Musique",
-                "nom" => "guitare",
-                "prix" => 149.99,
-                "description" => "Instrument",
-                "image" => "./guitar.jpg"
-            ],
-            [
-                "categorie" => "Voyage",
-                "nom" => "Adaptateur Universel Voyage",
-                "prix" => 29.99,
-                "description" => "Adaptateur de voyage compact.",
-                "image" => "charge.jpg"
-            ],
-            [
-                "categorie" => "Jeux vidéo",
-                "nom" => "Clavier Gaming",
-                "prix" => 99.99,
-                "description" => "Clavier",
-                "image" => "./key.jpg"
-            ],
-            [
-                "categorie" => "Maison",
-                "nom" => "Lampe de Bureau Moderne",
-                "prix" => 59.99,
-                "description" => "Lampe",
-                "image" => "./lamp.jpg"
-            ],
-            [
-                "categorie" => "Cuisine",
-                "nom" => "Machine à café",
-                "prix" => 79.99,
-                "description" => "Peinture",
-                "image" => "./coffee.jpg"
-            ],
-            [
-                "categorie" => "Musique",
-                "nom" => "Microphone Studio Pro",
-                "prix" => 129.99,
-                "description" => "Microphon deluxe",
-                "image" => "./mic.jpg"
-            ],
-            [
-                "categorie" => "Voyage",
-                "nom" => "Trousse de Toilette",
-                "prix" => 39.99,
-                "description" => "Trousse de toilette imperméable.",
-                "image" => "./waterproof.jpg"
-            ],
-            [
-                "categorie" => "Jeux vidéo",
-                "nom" => "Écran Gaming UltraWide",
-                "prix" => 499.99,
-                "description" => "Écran incurvé 34 pouces.",
-                "image" => "./screen.jpg"
-            ]
-        ];
+
+        require_once("./product.php");
+        
 
        
         $nouveauProduit = [
             "categorie" => "Maison",
             "nom" => "Aspirateur Robot CleanBot",
             "prix" => 299.99,
-            "description" => "Aspirateur robotisé avec cartographie intelligente et contrôle vocal.",
-            "image" => "./vaccuml.jpg"
+            "description" => "Aspirateur robotisé avec cartographie intelligente.",
+            "image" => "./img/vaccuml.jpg"
         ];
         $produits[] = $nouveauProduit;
 
         
-        echo '<div class="row bg-primary text-center">';
+        echo '<div class="row">';
         foreach ($produits as $item) {
+            // Format price using NumberFormatter
+            $fmt = new NumberFormatter('fr_FR', NumberFormatter::CURRENCY);
+            $formattedPrice = $fmt->formatCurrency($item["prix"], 'EUR');
+
             echo '
             <div class="col-md-4 mb-4">
-                <div class="card bg-dark rounded-3">
+                <div class="card  bg-dark rounded-3 text-center">
                     <img src="' . $item["image"] . '" class="card-img-top" alt="' . $item["nom"] . '">
                     <div class="card-body">
-                        <h5 class="card-title text-white">' . $item["nom"] . '</h5>
-                        <h6 class="card-subtitle mt-2 mb-2 text-primary fst-italic">' . $item["categorie"] . '</h6>
-                        <p class="card-text text-white">' . $item["description"] . '</p>
-                        <p class="card-text fw-bold text-decoration-underline text-white">Prix : ' . number_format($item["prix"], 2, ',', ' ') . ' €</p>
+                        <h3 class="mt-2 mb-2 text-primary fst-italic">' . $item["categorie"] . '</h3>
+                        <h5 class="mb-2 text-white">' . $item["nom"] . '</h5>
+                        <p class="text-white">' . $item["description"] . '</p>
+                        <p class="fw-bold text-decoration-underline text-white">Prix : ' . $formattedPrice . '</p>
                     </div>
                 </div>
             </div>';
---
+++ product.php
@@ -0,0 +1,90 @@
+<?php
+        
+        $produits = [
+            [
+                "categorie" => "Voyage",
+                "nom" => "PRO BAG",
+                "prix" => 89.99,
+                "description" => "Sac à dos anti-vol.",
+                "image" => "./img/bag.jpg"
+            ],
+            [
+                "categorie" => "Jeux vidéo",
+                "nom" => "Assassin's Creed Odyssey",
+                "prix" => 59.99,
+                "description" => "Exploration ouverte dans l'ancienne Grèce.",
+                "image" => "./img/assasins.jpg"
+            ],
+            [
+                "categorie" => "Maison",
+                "nom" => "Oreiller",
+                "prix" => 49.99,
+                "description" => "Très confortable.",
+                "image" => "./img/pillow.jpg"
+            ],
+            [
+                "categorie" => "Art",
+                "nom" => "Tablette Graphique",
+                "prix" => 199.99,
+                "description" => "Tablette graphique haute précision.",
+                "image" => "./img/tab.jpg"
+            ],
+            [
+                "categorie" => "Musique",
+                "nom" => "Guitare",
+                "prix" => 149.99,
+                "description" => "Instrument de musique.",
+                "image" => "./img/guitar.jpg"
+            ],
+            [
+                "categorie" => "Voyage",
+                "nom" => "Adaptateur Universel Voyage",
+                "prix" => 29.99,
+                "description" => "Adaptateur de voyage compact.",
+                "image" => "./img/charge.jpg"
+            ],
+            [
+                "categorie" => "Jeux vidéo",
+                "nom" => "Clavier Gaming",
+                "prix" => 99.99,
+                "description" => "Clavier mécanique pour gamers.",
+                "image" => "./img/key.jpg"
+            ],
+            [
+                "categorie" => "Maison",
+                "nom" => "Lampe de Bureau Moderne",
+                "prix" => 59.99,
+                "description" => "Lampe LED réglable.",
+                "image" => "./img/lamp.jpg"
+            ],
+            [
+                "categorie" => "Cuisine",
+                "nom" => "Machine à café",
+                "prix" => 79.99,
+                "description" => "Machine à café automatique.",
+                "image" => "./img/coffee.jpg"
+            ],
+            [
+                "categorie" => "Musique",
+                "nom" => "Microphone Studio Pro",
+                "prix" => 129.99,
+                "description" => "Microphone de studio professionnel.",
+                "image" => "./img/mic.jpg"
+            ],
+            [
+                "categorie" => "Voyage",
+                "nom" => "Trousse de Toilette",
+                "prix" => 39.99,
+                "description" => "Trousse de toilette imperméable.",
+                "image" => "./img/waterproof.jpg"
+            ],
+            [
+                "categorie" => "Jeux vidéo",
+                "nom" => "Écran Gaming UltraWide",
+                "prix" => 499.99,
+                "description" => "Écran incurvé 34 pouces.",
+                "image" => "./img/screen.jpg"
+            ]
+        ];
+
+        ?>
